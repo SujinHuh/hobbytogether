fe4f8c04fc997ef1113a125f5518e85b6a3cdd30
SujinHuh <juin483@naver.com>
2020-03-22 10:43:56 +0900

    Hobby Together Project Start

---
+++ src/test/java/com/hobbytogther/HobbytogtherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hobbytogther;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbytogtherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
