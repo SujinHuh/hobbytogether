e02037dd629e7cf3aecd5055f51ef245f9162269
SujinHuh <juin483@naver.com>
2020-03-22 10:57:15 +0900

    Account domain class

---
+++ src/main/java/com/hobbytogther/domain/Account.java
@@ -0,0 +1,63 @@
+package com.hobbytogther.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+import static javax.persistence.FetchType.EAGER;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    // 이메일 인증 절차 (중복 이메일 확인)
+    private boolean emailVerified;
+
+    // 이메일 인증 토큰 값
+    private String emailCheckToken;
+
+    private LocalDateTime joinedAt;
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location;
+
+    @Lob
+    @Basic(fetch = EAGER)
+    //lob -> text 타입에 맵핑 이됨
+    private String profileImage;
+
+    private boolean studyCreateByEmail;
+
+    private boolean studyCreateByWeb;
+
+    private boolean studyEnrollmentResultByEmail;
+
+    private boolean studyEnrollmentResultByWeb;
+
+    private boolean studyUpdateByEmail;
+
+    private boolean studyUpdateByWeb;
+}
