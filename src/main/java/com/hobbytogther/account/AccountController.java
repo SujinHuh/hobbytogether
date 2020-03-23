f2c8e6b031e12210bbdbea2125045b1345c01b48
SujinHuh <juin483@naver.com>
2020-03-23 11:37:44 +0900

    signUpFormValidator   @InitBinder 사용

--- src/main/java/com/hobbytogther/account/AccountController.java
+++ src/main/java/com/hobbytogther/account/AccountController.java
@@ -1,27 +1,47 @@
 package com.hobbytogther.account;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final SignUpFormValidator signUpFormValidator;
+
+    /**
+     * @param webDataBinder
+     * signUpFormValidator.validate(signUpForm, errors);
+     *         if(errors.hasErrors()) {
+     *             return "account/sign-up";
+     *         }
+     *   동일
+     */
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
+    }
 
     @GetMapping("/sign-up")
     public String signUpFrom(Model model) {
-        model.addAttribute("signUpForm",new SignUpForm());
+        model.addAttribute("signUpForm", new SignUpForm());
         return "account/sign-up";
     }
 
     @PostMapping("/sign-up")
     public String signUpSubmit(@Valid SignUpForm signUpForm, BindingResult errors) {
-        if(errors.hasErrors()) {
+        if (errors.hasErrors()) {
             return "account/sign-up";
         }
+
         // TODO 회원 가입 처리
         return "redirect:/";
     }
