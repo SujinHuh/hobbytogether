6ac7c76103f05d13e8216d479fa605060e5a2cb4
SujinHuh <juin483@naver.com>
2020-03-23 10:57:14 +0900

    signUpSubmit Valid

--- src/main/java/com/hobbytogther/account/AccountController.java
+++ src/main/java/com/hobbytogther/account/AccountController.java
@@ -2,14 +2,28 @@ package com.hobbytogther.account;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class AccountController {
 
     @GetMapping("/sign-up")
     public String signUpFrom(Model model) {
-        model.addAttribute("signUpForm",new signUpForm());
+        model.addAttribute("signUpForm",new SignUpForm());
         return "account/sign-up";
     }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, BindingResult errors) {
+        if(errors.hasErrors()) {
+            return "account/sign-up";
+        }
+        // TODO 회원 가입 처리
+        return "redirect:/";
+    }
+
 }
