e10c3dd4ad924274e76cd686187555b771141ef7
SujinHuh <juin483@naver.com>
2020-03-25 14:31:35 +0900

    signUp CheckEmail - Test

--- src/test/java/com/hobbytogther/account/AccountControllerTest.java
+++ src/test/java/com/hobbytogther/account/AccountControllerTest.java
@@ -1,6 +1,7 @@
 package com.hobbytogther.account;
 
 import com.hobbytogther.domain.Account;
+import org.hibernate.tool.schema.internal.exec.JdbcConnectionAccessConnectionProviderImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.then;
 
+import static org.mockito.Mockito.mock;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -35,6 +37,56 @@ class AccountControllerTest {
 
     @MockBean
     JavaMailSender javaMailSender;
+
+    @DisplayName("회원원가입 인증 메일 - 입력값 오류")
+    @Test
+    public void checkEmailToken_ErrorInPut() throws Exception {
+        /**
+         * - 입력 값이 잘못 된
+         *      1. error 프로퍼티가 model에 들어있는지 확인
+         *      2, 뷰 이름이 account/checked-email 인지 확인
+         */
+        mockMvc.perform(get("/check-email-token")
+                .param("token", "aldkfjed")
+                .param("email", "Test@email.con"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("error"))
+                .andExpect(view().name("account/checked-email"));
+    }
+
+
+    @DisplayName("회원원가입 인증 메일 - 입력값 정상")
+    @Test
+    public void checkEmailToken_CorrectInPut() throws Exception {
+        /**
+         * - 입력 값이 올바른
+         *   1. 모델에 error가 없는지 확인
+         *   2. 모델에 numberOuUser가 있는지 확인
+         *   3. 모델에 nickname이 있는지 확인
+         *   4. 뷰 이름 확인하기
+         */
+        Account account = Account.builder()
+                .email("tetst@mail.com")
+                .password("12345dac")
+                .nickname("sujin")
+                .build()
+                ;
+
+        Account newAccount = accountRepository.save(account);
+        newAccount.generateEmailCheckToken();
+
+        mockMvc.perform(get("/check-email-token")
+                .param("token", newAccount.getEmailCheckToken())
+                .param("email", newAccount.getEmail()))
+                .andExpect(model().attributeExists("numberOfUser"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeDoesNotExist("error"))
+                .andExpect(model().attributeExists("nickname"))
+                .andExpect(model().attributeExists("numberOfUser"))
+                .andExpect(view().name("account/checked-email"));
+
+    }
+
 
     @DisplayName("회원원가입 화면 정상화면 보이는지 확인")
     @Test
@@ -87,8 +139,8 @@ class AccountControllerTest {
         //when
         Account account = accountRepository.findByEmail("test@naver.com");
         assertNotNull(account);
-        assertNotEquals(account.getPassword(),"1231");
-        assertNotEquals(account,"test@naver.com");
+        assertNotEquals(account.getPassword(), "1231");
+        assertNotEquals(account, "test@naver.com");
         //then
         then(javaMailSender).should().send(any(SimpleMailMessage.class)); //아무런 인스턴스 타입으로 Send 호출이 되었는가 확인
     }
