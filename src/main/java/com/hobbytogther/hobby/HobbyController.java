02023366adf019df9f6d641f2a8dc6595db9d357
SujinHuh <juin483@naver.com>
2020-04-12 22:31:16 +0900

    View Hobby (진행중)

--- src/main/java/com/hobbytogther/hobby/HobbyController.java
+++ src/main/java/com/hobbytogther/hobby/HobbyController.java
@@ -8,12 +8,14 @@ import com.hobbytogther.hobby.validator.HobbyFormValidator;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.propertyeditors.URLEditor;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -27,25 +29,40 @@ public class HobbyController {
     private final HobbyService hobbyService;
     private final ModelMapper modelMapper;
     private final HobbyFormValidator hobbyFormValidator;
+    private final HobbyRepository hobbyRepository;
 
-    @InitBinder("hobbyForm") /** Valid */
+    @InitBinder("hobbyForm")
+    /** Valid */
     public void hobbyFormInitBinder(WebDataBinder webDataBinder) {
         webDataBinder.addValidators(hobbyFormValidator);
     }
 
     @GetMapping("/new-hobby")
-    public String newStudyForm(@CurrentAccount Account account , Model model) {
+    public String newStudyForm(@CurrentAccount Account account, Model model) {
         model.addAttribute(account);
         model.addAttribute(new HobbyForm());
         return "hobby/form";
     }
+
     @PostMapping("/new-hobby")
     public String newHobby(@CurrentAccount Account account, @Valid HobbyForm hobbyForm, Errors errors) {
-        if(errors.hasErrors()) {
+        if (errors.hasErrors()) {
             return "hobby/form";
         }
-        Hobby newHobby = hobbyService.createNewHobby(modelMapper.map(hobbyForm, Hobby.class),account);
+        Hobby newHobby = hobbyService.createNewHobby(modelMapper.map(hobbyForm, Hobby.class), account);
         //Post -> redirect / 해당하는 URL로 보낼 것
-        return "redirect:/hobby" + URLEncoder.encode(newHobby.getPath(), StandardCharsets.UTF_8);
+        return "redirect:/hobby/" + URLEncoder.encode(newHobby.getPath(), StandardCharsets.UTF_8);
     }
+
+
+    /**
+     * Hobby 조회
+     */
+    @GetMapping("/hobby/{path}")
+    public String viewHobby(@CurrentAccount Account account, @PathVariable String path, Model model) {
+        model.addAttribute(account);
+        model.addAttribute(hobbyRepository.findByPath(path));
+        return "hobby/view";
+    }
+
 }
