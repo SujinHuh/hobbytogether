89936b4401e9b94704f5023cbe64e838ed05f845
SujinHuh <juin483@naver.com>
2020-04-13 13:33:23 +0900

    View Hobby (완료)

--- src/main/java/com/hobbytogther/domain/Hobby.java
+++ src/main/java/com/hobbytogther/domain/Hobby.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
-@NamedEntityGraph(name = "Study.withAll", attributeNodes = {
+@NamedEntityGraph(name = "Hobby.withAll", attributeNodes = {
         @NamedAttributeNode("tags"),
         @NamedAttributeNode("zones"),
         @NamedAttributeNode("managers"),
--- src/main/java/com/hobbytogther/hobby/HobbyController.java
+++ src/main/java/com/hobbytogther/hobby/HobbyController.java
@@ -55,9 +55,7 @@ public class HobbyController {
     }
 
 
-    /**
-     * Hobby 조회
-     */
+    /** Hobby 조회*/
     @GetMapping("/hobby/{path}")
     public String viewHobby(@CurrentAccount Account account, @PathVariable String path, Model model) {
         model.addAttribute(account);
