b555f3c93b51dd4e1128e284b3a54e4ead7adc5f
huhsujin <juin483@naver.com>
2020-05-10 15:19:11 +0900

    Error handling

---
+++ src/main/java/com/hobbytogther/modules/main/ExceptionAdvice.java
@@ -0,0 +1,25 @@
+package com.hobbytogther.modules.main;
+
+import com.hobbytogther.modules.account.Account;
+import com.hobbytogther.modules.account.CurrentAccount;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@ControllerAdvice
+public class ExceptionAdvice {
+//Error 로그남김 , 파악을 위해서
+    @ExceptionHandler
+    public String handleRuntimeException(@CurrentAccount Account account, HttpServletRequest req, RuntimeException e) {
+        if (account != null) {
+            log.info("'{}' requested '{}'", account.getNickname(), req.getRequestURI());
+        } else {
+            log.info("requested '{}'", req.getRequestURI());
+        }
+        log.error("bad request", e);
+        return "error";
+    }
+}
