061548c55ccd0c04bb013119ceb552b5ae9fc898
huhsujin <juin483@naver.com>
2020-05-22 14:21:50 +0900

    Main page 수정

--- src/main/java/com/hobbytogther/modules/event/EnrollmentRepository.java
+++ src/main/java/com/hobbytogther/modules/event/EnrollmentRepository.java
@@ -3,9 +3,11 @@ package com.hobbytogther.modules.event;
 import com.hobbytogther.modules.account.Account;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Transactional(readOnly = true)
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
     boolean existsByEventAndAccount(Event event, Account account);
 
